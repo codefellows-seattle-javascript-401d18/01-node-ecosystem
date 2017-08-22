a67e94b30073c900a409a9eda548477c0af33c5f
GavinThomas1192 <gthomas1192@gmail.com>
2017-08-22 09:44:03 -0700

    fixed some issues

--- lab-gavin/__test__/greet.test.js
+++ lab-gavin/__test__/greet.test.js
@@ -3,10 +3,22 @@ const greet = require('../lib/greet');
 
 
 
-describe('greet.js', function() {
+// describe('greet.js', function() {
+//
+//   describe('default properties', () => {
+//     test('should have hello concatenated with the input', () => {
+//
+//       expect(greet('world')).toEqual('hello world.');
+//     });
+//   });
+// });
 
-  describe('default properties', () => {
-    test('should have hello concatenated with the input', () => {
+
+
+describe('index.js', function() {
+
+  describe('Default properties', () => {
+    test('should return hello concatenated with the argument', () => {
 
       expect(greet('world')).toEqual('hello world.');
     });
