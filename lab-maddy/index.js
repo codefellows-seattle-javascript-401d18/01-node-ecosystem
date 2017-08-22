627c139cb7a151431c8ac13588dd25795aacce93
Madeline Stevens <stevensm1987@gmail.com>
2017-08-22 11:57:48 -0700

    deleted the node modules directory

---
+++ lab-maddy/index.js
@@ -0,0 +1,9 @@
+'use strict';
+//this file is the entry point. this is what node executes.
+
+const math = require('./lib/arithmetic');
+const greet = require('./lib/greet');
+
+console.log('should return hello Freddy' newName.greet('Freddy'));
+console.log('should return the sum of 2 and 2 which is 4' math.add(2,2));
+console.log('should return the difference between 6 and 4 which is 2' math.sub(6,4));
