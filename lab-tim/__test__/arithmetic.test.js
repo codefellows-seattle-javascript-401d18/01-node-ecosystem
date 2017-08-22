bfdefee3fc3c005411f5372493796198f33b1059
Tim Turner <timmyturner@me.com>
2017-08-22 10:14:24 -0700

    refactors the math functions and testing

--- lab-tim/__test__/arithmetic.test.js
+++ lab-tim/__test__/arithmetic.test.js
@@ -1,14 +1,26 @@
 'use strict';
 
+// create an add (2 integers) and sub (2 integers) method
+// return the sum or difference respectively
+// if arguments are not numbers, return error
+
 const arithmetic = require('../lib/arithmetic');
 
 describe('arithmetic.js', function() {
-  test('returns numbers added/subtracted', () => {
-    expect(arithmetic.add(10, 5)).toEqual(15)
-    expect(arithmetic.sub(10, 5)).toEqual(5)
+  describe('#add', () => {
+    test('returns numbers added', () => {
+      expect(arithmetic.add(10, 5)).toEqual(15)
+    })
+    test('returns error message if arguments are not numbers', () => {
+      expect(arithmetic.add('bob', 'jim')).toEqual('Please enter 2 numbers')
+    })
   })
-  test('if not numbers return error message', () => {
-    expect(arithmetic.add('bob', 'jim')).toEqual('Please enter 2 numbers')
-    expect(arithmetic.sub('bob', 'jim')).toEqual('Please enter 2 numbers')
+  describe('#sub', () => {
+    test('returns numbers subtracted', () => {
+      expect(arithmetic.sub(10, 5)).toEqual(5)
+    })
+    test('returns NaN if arguments are not numbers', () => {
+      expect(arithmetic.sub('bob', 'jim')).toEqual(NaN)
+    })
   })
 })
