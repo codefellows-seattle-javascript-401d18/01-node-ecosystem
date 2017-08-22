750a1a4a90538ebaed4cc4a7a302b1ead081f507
GavinThomas1192 <gthomas1192@gmail.com>
2017-08-22 09:58:26 -0700

    almost ready for turn in

--- lab-gavin/__test__/greet.test.js
+++ lab-gavin/__test__/greet.test.js
@@ -19,8 +19,11 @@ describe('index.js', function() {
 
   describe('Default properties', () => {
     test('should return hello concatenated with the argument', () => {
-
       expect(greet('world')).toEqual('hello world.');
+    });
+
+    test('should return null if arg not a string', () => {
+      expect(greet(1)).toBeNull();
     });
   });
 });
