c35a858a1cfee26b50028631b3c653cfc1b070ec
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-22 18:37:28 -0700

    ugh

--- lab-shaun/index.js
+++ lab-shaun/index.js
@@ -5,5 +5,5 @@ const arithmetic = require ('../lib/arithmetic');
 const greet = require ('../lib/greet');
 
 console.log(greet('world'));
-console.log(arithmetic.add(3, 3));
+console.log(arithmetic.add(3, 5));
 console.log(arithmetic.sub(54, 32));
